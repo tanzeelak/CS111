076a3768762bc2fe4f77156dc6cc7f4846fd6b46
tanzeelak <tnkhan8042@gmail.com>
2017-04-06 21:15:25 -0700

    removed old version, dummy options work

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -3,78 +3,88 @@
 #include <getopt.h>
 #include <stdio.h>
 #include <signal.h>
-
+#include <fctl.h>
 
 void seghandle(int signum) {
   fprintf(stderr, "Segment fault");
 	  exit(2);
 }
 
+static int verbose_flag;
 
 int main(int argc, char* argv[])
 {
-  struct option long_options[] = {
+  
+  int c;
+  
+  while (1)
+    {
+      static struct option long_options[] = {
     {"input", required_argument, 0, 'i'},
     {"output", required_argument, 0, 'o'},
     {"segfault", no_argument, 0, 's'},
     {"catch", no_argument, 0, 'c'},
     {0,0,0,0}
-  };
+      };
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
 
-  int option;
-  int fd0, fd1;
-  FILE *file;
-  file = fopen(argv[1], "r");
-  int option_index = 0;
-  if (file) {
-    while((option = getopt_long(argc, argv, "i:o:sc", long_options, 0)) != -1)
-      {
-        switch(option)
-          {
-	  case 0:
-	    if (long_options[option_index].flag != 0)
-	      break;
-	    printf("option %s", long_options[option_index].name);
-	    if (optarg)
-	      printf(" with arg %s", optarg);
-	    printf ("\n");
-	    break;
-          case 'i':
-	    if ((file = fopen(argv[1], "r")) != NULL){
-	      printf("case i");
-              }
-              else {
-                perror("Error opening file");
-                exit(2);
-		  }
-	      break;
-	    case 'o':
-	      fd1 = creat(optarg, 0666);
-	      if (fd1 > -1)
-		{
-		  printf("case o");
-		  
-		}
-	      else {
-		perror("Error creating file");
-		exit(2);
-	      }
-	      break;
-	    case 's':
-	      printf("case s");
-	      char fail = NULL;
-	      break;
-	    case 'c':
-	      printf("case c");
-	      signal(SIGSEGV, seghandle);
-	      break;
-	  default: 
-	    abort();
-	    break;
-	      }
+      c = getopt_long (argc, argv, "i:o:sc:",
+                       long_options, &option_index);
 
-	  }
-  }
-    exit(0);
+      /* Detect the end of the options. */
+      if (c == -1)
+        break;
 
-  }
+      switch (c)
+        {
+        case 0:
+          /* If this option set a flag, do nothing else now. */
+          if (long_options[option_index].flag != 0)
+            break;
+          printf ("option %s", long_options[option_index].name);
+          if (optarg)
+            printf (" with arg %s", optarg);
+          printf ("\n");
+          break;
+
+        case 'i':
+          puts ("option -i\n");
+          break;
+
+        case 'o':
+          puts ("option -o\n");
+          break;
+
+        case 's':
+          printf ("option -s with value `%s'\n", optarg);
+          break;
+
+        case 'c':
+          printf ("option -c with value `%s'\n", optarg);
+          break;
+
+        default:
+          abort ();
+        }
+    }
+
+  /* Instead of --
+     -- as they are encountered,
+     we report the final status resulting from them. */
+  if (verbose_flag)
+    puts ("verbose flag is set");
+
+  /* Print any remaining command line arguments (not options). */
+  if (optind < argc)
+    {
+      printf ("non-option ARGV-elements: ");
+      while (optind < argc)
+        printf ("%s ", argv[optind++]);
+      putchar ('\n');
+    }
+
+  exit (0);
+  
+
+}
