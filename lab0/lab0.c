5c3d7f2d3791ce5a59e2fe1b1752757d843a8f1a
tanzeelak <tnkhan8042@gmail.com>
2017-04-06 20:38:01 -0700

    less warnings still doesn't work with options

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -25,14 +25,23 @@ int main(int argc, char* argv[])
   int fd0, fd1;
   FILE *file;
   file = fopen(argv[1], "r");
+  int option_index = 0;
   if (file) {
-    while(option = getopt_long(argc, argv, "i:o:sc", long_options, 0) != -1)
+    while((option = getopt_long(argc, argv, "i:o:sc", long_options, 0)) != -1)
       {
         switch(option)
           {
+	  case 0:
+	    if (long_options[option_index].flag != 0)
+	      break;
+	    printf("option %s", long_options[option_index].name);
+	    if (optarg)
+	      printf(" with arg %s", optarg);
+	    printf ("\n");
+	    break;
           case 'i':
-            if (file = fopen(argv[1], "r") != -1){
-
+	    if ((file = fopen(argv[1], "r")) != NULL){
+	      printf("case i");
               }
               else {
                 perror("Error opening file");
@@ -43,6 +52,7 @@ int main(int argc, char* argv[])
 	      fd1 = creat(optarg, 0666);
 	      if (fd1 > -1)
 		{
+		  printf("case o");
 		  
 		}
 	      else {
@@ -51,11 +61,16 @@ int main(int argc, char* argv[])
 	      }
 	      break;
 	    case 's':
-	      option = NULL;
+	      printf("case s");
+	      char fail = NULL;
 	      break;
 	    case 'c':
+	      printf("case c");
 	      signal(SIGSEGV, seghandle);
 	      break;
+	  default: 
+	    abort();
+	    break;
 	      }
 
 	  }
