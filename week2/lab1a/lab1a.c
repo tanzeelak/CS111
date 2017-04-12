609f189f102f8a4e95822c27d83a5ed00699a88f
tanzeelak <tnkhan8042@gmail.com>
2017-04-12 15:20:34 -0700

    compiles with poll prob doesn't do anything

--- week2/lab1a/lab1a.c
+++ week2/lab1a/lab1a.c
@@ -2,14 +2,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include <string.h>
 #include <poll.h>
 
 struct termios initState;
+struct pollfd fds[1];
+int timeout_msecs = 500;
+int ret;
+int i;
 
-int main()
+
+int main(int argc, char* argv[])
 {
   int result;
+  fds[0].fd = open(optarg, O_RDONLY);
+  fds[0].events = POLLOUT | POLLWRBAND;
 
   result = tcgetattr (1, &initState);
   if (result < 0)
@@ -22,6 +30,27 @@ int main()
   newState.c_oflag = 0;
   newState.c_lflag = 0;
   
+  ret = poll(fds, 1, timeout_msecs);
+  char buff;
+  if (ret > 0) {
+    /* An event on one of the fds has occurred. */
+    for (i=0; i<2; i++) {
+      if (fds[i].revents & POLLWRBAND) {
+        /* Priority data may be written on device number i. */
+	while(read(0, &buff, 1))
+	  {
+	    write(1, &buff, 1);
+	  }
+      }
+      if (fds[i].revents & POLLOUT) {
+        /* Data may be written on device number i. */
+      }
+      if (fds[i].revents & POLLHUP) {
+        /* A hangup has occurred on device number i. */
+      }
+    }
+  }
+
  
   //result = tcsetattr (desc, TCSANOW, &initState);
   // if (result < 0)
