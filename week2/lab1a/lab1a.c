58c6d05531b790be5622b1b9f912e8f9378587fc
tanzeelak <tnkhan8042@gmail.com>
2017-04-13 21:37:00 -0700

    catch error for currnet version

--- week2/lab1a/lab1a.c
+++ week2/lab1a/lab1a.c
@@ -7,6 +7,7 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 /* Use this variable to remember original terminal attributes. */
 
@@ -49,24 +50,30 @@ main (void)
   char c;
 
   set_input_mode ();
-
+  int rfd;
   while (1)
     {
-      read (STDIN_FILENO, &c, 1);
-      if (c == '\004')          /* C-d */
-	{  
-	  reset_input_mode();
-	  break;
-	}
-      else if (c == '\n' || c == '\r')
-      {
-        char temp[2] = {'\r', '\n'};
-        write(1, &temp, 2);
-	//putchar(&temp);
-      }     
-      else
-	write(1, &c, 1);
-      //putchar (c);
+      rfd = read (STDIN_FILENO, &c, 1);
+      if (rfd >= 0) {
+	if (c == '\004')          /* C-d */
+	  {  
+	    reset_input_mode();
+	    break;
+	  }
+	else if (c == '\n' || c == '\r')
+	  {
+	    char temp[2] = {'\r', '\n'};
+	    write(1, &temp, 2);
+	    //putchar(&temp);
+	  }     
+	else
+	  write(1, &c, 1);
+	//putchar (c);
+      }
+      else {
+	fprintf(stderr, "Failed to read file. %s\n", strerror(errno));
+	exit(2);
+      }
     }
 
   return EXIT_SUCCESS;
