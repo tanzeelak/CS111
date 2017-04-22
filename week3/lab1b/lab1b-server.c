b6ac3cd3a11104472082ad8e96069b3767f33a37
tanzeelak <tnkhan8042@gmail.com>
2017-04-22 00:37:18 -0700

    from base socket code

--- week3/lab1b/lab1b-server.c
+++ week3/lab1b/lab1b-server.c
@@ -1,43 +1,72 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
 #include <string.h>
-#include <sys/types.h>
-#include <time.h> 
 
-int main(int argc, char *argv[])
-{
-  int listenfd = 0, connfd = 0;
-  struct sockaddr_in serv_addr; 
-
-  char sendBuff[1025];
-  time_t ticks; 
-
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&serv_addr, '0', sizeof(serv_addr));
-  memset(sendBuff, '0', sizeof(sendBuff)); 
-
+int main( int argc, char *argv[] ) {
+  int sockfd, newsockfd, portno, clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int  n;
+   
+  /* First call to socket() function */
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+  if (sockfd < 0) {
+    perror("ERROR opening socket");
+    exit(1);
+  }
+   
+  /* Initialize socket structure */
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = 5001;
+   
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_addr.sin_port = htons(5000); 
-
-  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
-
-  listen(listenfd, 10); 
-
-  while(1)
-    {
-      connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
-
-      ticks = time(NULL);
-      snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
-      write(connfd, sendBuff, strlen(sendBuff)); 
-
-      close(connfd);
-      sleep(1);
-    }
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+   
+  /* Now bind the host address using bind() call.*/
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR on binding");
+    exit(1);
+  }
+      
+  /* Now start listening for the clients, here process will
+   * go in sleep mode and will wait for the incoming connection
+   */
+   
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+   
+  /* Accept actual connection from the client */
+  newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+  
+  if (newsockfd < 0) {
+    perror("ERROR on accept");
+    exit(1);
+  }
+   
+  /* If connection is established then start communicating */
+  bzero(buffer,256);
+  n = read( newsockfd,buffer,255 );
+   
+  if (n < 0) {
+    perror("ERROR reading from socket");
+    exit(1);
+  }
+   
+  printf("Here is the message: %s\n",buffer);
+   
+  /* Write a response to the client */
+  n = write(newsockfd,"I got your message",18);
+   
+  if (n < 0) {
+    perror("ERROR writing to socket");
+    exit(1);
+  }
+      
+  return 0;
 }
