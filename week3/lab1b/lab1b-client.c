25121d9594e34ba4a211bf69645e6e274d23860f
tanzeelak <tnkhan8042@gmail.com>
2017-04-25 02:42:10 -0700

    client has working mcrypt

--- week3/lab1b/lab1b-client.c
+++ week3/lab1b/lab1b-client.c
@@ -12,11 +12,11 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <netdb.h>
 #include <netinet/in.h>
-
 #include <string.h>
+#include <mcrypt.h>
+
 #define h_addr h_addr_list[0] /* for backward compatibility */
 
 struct termios saved_attributes;
@@ -211,6 +211,58 @@ int main(int argc, char *argv[]) {
 	    }
 	}
 
+
+      //encryption
+
+      MCRYPT td;
+      int i;
+      char *key;
+      char password[20];
+      //      char block_buffer;
+      char *IV;
+      int keysize=19; /* 128 bits */
+
+      key=calloc(1, keysize);
+      strcpy(password, "A_large_key");
+
+      /* Generate the key using the password */
+      /*  mhash_keygen( KEYGEN_MCRYPT, MHASH_MD5, key, keysize, NULL, 0, password, strlen(password));
+       */
+      memmove( key, password, strlen(password));
+
+      td = mcrypt_module_open("twofish", NULL, "cfb", NULL);
+      if (td==MCRYPT_FAILED) {
+	return 1;
+      }
+      IV = malloc(mcrypt_enc_get_iv_size(td));
+
+      /* Put random data in IV. Note these are not real random data, 
+       * consider using /dev/random or /dev/urandom.
+       */
+
+      /*  srand(time(0)); */
+      for (i=0; i< mcrypt_enc_get_iv_size( td); i++) {
+	IV[i]=rand();
+      }
+
+      i=mcrypt_generic_init( td, key, keysize, IV);
+      if (i<0) {
+	mcrypt_perror(i);
+	return 1;
+      }
+
+
+     
+	mcrypt_generic (td, &buffer, rfd);
+
+	/* Comment above and uncomment this to decrypt */
+	/*    mdecrypt_generic (td, &block_buffer, 1);  */
+
+
+     
+      mcrypt_generic_deinit(td);
+
+      mcrypt_module_close(td);
 
 
 
