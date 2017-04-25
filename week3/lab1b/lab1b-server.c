793b2d9cf571e5d52847bb0108f5b8d3910546fd
tanzeelak <tnkhan8042@gmail.com>
2017-04-24 20:14:11 -0700

    can send to shell

--- week3/lab1b/lab1b-server.c
+++ week3/lab1b/lab1b-server.c
@@ -1,3 +1,15 @@
+#include <termios.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,10 +20,22 @@
 
 int to_child_pipe[2];
 int from_child_pipe[2];
+pid_t pid = -1;
+pid_t w;
+int status;
+int newsockfd;
+char buffer[2048];
+
+void sysFailed(char* sysCall, int exitNum)
+{
+  fprintf(stderr, "%s failed: %s\n", sysCall, strerror(errno));
+  exit(exitNum);
+}
+
 
 int main( int argc, char *argv[] ) {
-  int sockfd, newsockfd, portno, clilen;
-  char buffer[256];
+  int sockfd, portno, clilen;
+  //  char buffer[256];
   struct sockaddr_in serv_addr, cli_addr;
   int  n;
    
@@ -53,9 +77,145 @@ int main( int argc, char *argv[] ) {
   }
   while(1) {   
   /* If connection is established then start communicating */
-  bzero(buffer,256);
-  n = read( newsockfd,buffer,255 );
-   
+  bzero(buffer,2048);
+  //  n = read( newsockfd,buffer,255 );
+  
+
+  if (pipe(to_child_pipe) == -1)
+    {
+      fprintf(stderr, "pipe() failed!\n");
+      exit(1);
+    }
+  if (pipe(from_child_pipe) == -1) 
+    {
+      fprintf(stderr, "pipe() failed! \n");
+      exit(1);
+    }
+
+  if ((pid = fork()) == -1)
+    {
+      sysFailed("fork", 1);
+    }
+
+  if (pid > 0)//parent
+    {
+
+      if (close(to_child_pipe[0]) == -1)
+	{
+	  sysFailed("close", 1);
+	}
+      
+      if (close(from_child_pipe[1]) == -1)
+	{
+	  sysFailed("close", 1);
+	}
+
+      for (;;) {
+	int count = 0;
+
+	//reading from keyboard
+
+	  if ((count = read(newsockfd, buffer, 2048)) == -1)
+	    {
+	      sysFailed("Read", 1);
+	    }
+
+          int i;
+          for (i = 0; i < count; i++)
+            {
+
+		if (write(1, &buffer[i], 1) == -1)
+		  {
+		    sysFailed("write", 1);
+		  }
+
+            }
+	  //forward to shell
+	  if (write(to_child_pipe[1], buffer, count) == -1)
+	    {
+	      sysFailed("write", 1);
+	    }
+
+
+	
+	//reading from shell
+
+
+	  if ((count = read(from_child_pipe[0], buffer, 2048)) == -1)
+	    {
+	      sysFailed("read", 1);
+	    }
+	  int j;
+	  for (j = 0; j < count; j++)
+	    {
+
+		
+		if (write(1, &buffer[j], 1) == -1)
+		  {
+		    sysFailed("write", 1);
+		  }
+
+	    }
+
+	  write(newsockfd, buffer, 2048);
+      }
+    }
+  else if (pid == 0) {
+
+    if (close(to_child_pipe[1]) == -1)
+      {
+	sysFailed("close", 1);
+      }
+    
+    if (close(from_child_pipe[0]) == -1)
+      {
+	sysFailed("close", 1);
+      }
+    
+    if (dup2(to_child_pipe[0], STDIN_FILENO) == -1)
+      {
+	sysFailed("dup2", 1);
+      }
+
+    if (dup2(from_child_pipe[1], STDOUT_FILENO) == -1)
+      {
+	sysFailed("dup2", 1);
+      }
+
+    if (dup2(from_child_pipe[1], 2) == -1)
+      {
+	sysFailed("dup2", 1);
+      }
+
+    if (close(to_child_pipe[0]) == -1)
+      {
+	sysFailed("close", 1);
+      }
+    
+    if (close(from_child_pipe[1]) == -1)
+      {
+	sysFailed("close", 1);
+      }
+    char *execvp_argv[2];
+    char execvp_filename[] = "/bin/bash";
+    execvp_argv[0] = execvp_filename;
+    execvp_argv[1] = NULL;
+    if (execvp(execvp_filename, execvp_argv) == -1) 
+      {
+	fprintf(stderr, "execvp() failed!\n");
+	exit(1);
+      }
+    
+  }
+  else {
+    fprintf(stderr, "fork() failed!\n");
+    exit(1);
+  }
+
+
+
+
+ 
   if (n < 0) {
     perror("ERROR reading from socket");
     exit(1);
@@ -64,7 +224,7 @@ int main( int argc, char *argv[] ) {
   printf("Here is the message: %s\n",buffer);
    
   /* Write a response to the client */
-  n = write(newsockfd,"I got your message",18);
+  //  n = write(newsockfd,"I got your message",18);
    
   if (n < 0) {
     perror("ERROR writing to socket");
