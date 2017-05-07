fe511310863cdd3e0956fea0fcfbd1915fea4420
tanzeelak <tnkhan8042@gmail.com>
2017-05-07 01:24:34 -0700

    added locks appear to work

--- labs/lab2a/lab2_add.c
+++ labs/lab2a/lab2_add.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include <string.h>
 #include <time.h>
+#include <string.h>
 
 #define print_err() do {if (errno) {fprintf(stderr, "error %s", strerr(errno)); exit(1);}} while(0)
 
@@ -13,11 +14,9 @@ long long count;
 int threadNum = 1;
 int iterNum = 1;
 int opt_yield = 0;
+int testAndSet = 0;
+char syncopt = NULL;
 
-void addNone(long long *pointer, long long value) {
-  long long sum = *pointer + value;
-  *pointer = sum;
-}
 
 void add(long long *pointer, long long value) {
   long long sum = *pointer + value;
@@ -26,34 +25,51 @@ void add(long long *pointer, long long value) {
   *pointer = sum;
 }
 
-/*
-void fakeAdd(struct Data *datPass)
-{
-  int i;
+void add_c(long long *pointer, long long value) {
+  long long oldValue;
+  long long sum;
+  do {
+    oldValue = *pointer;
+    sum = oldValue + value;
+    if (opt_yield)
+      sched_yield();
+  }while(__sync_val_compare_and_swap(pointer, oldValue, sum) != oldValue);
 
-  for (i = 0; i < datPass->iterNum; i++)
-    {
-      ++(*(datPass->ptr));
-      if (opt_yield)
-	sched_yield();
-    }
-  for (i = 0; i < datPass->iterNum; i++)
-    {    
-      --(*(datPass->ptr));
-      if (opt_yield)
-	sched_yield();
-    }
- 
 }
-*/
+
 void* threadAdd(void* ptr)
 {
   int i;
   long long* counter = (long long*)ptr;
+
   for (i = 0; i < iterNum; i++)
     {
-      add(counter, 1);
-      add(counter, -1);
+      perror("pls");
+      if (syncopt == 'm')
+	{
+	  perror("wautp");
+	  pthread_mutex_lock(&count_mutex);
+	  add(counter, 1);
+	  add(counter, -1);
+	  pthread_mutex_unlock(&count_mutex);
+	}	
+      else if (syncopt == 's')
+	{
+  	  while(__sync_lock_test_and_set(&testAndSet, 1));
+	  add(counter, 1);
+	  add(counter, -1);
+	  __sync_lock_release(&testAndSet);
+	}
+      else if (syncopt == 'c')
+	{
+	  
+	}
+      else 
+	{
+	  add(counter, 1);
+	  add(counter, -1);
+	}
+      
     }
 
 }
@@ -65,6 +81,7 @@ int main(int argc, char *argv[])
     int threadFlag = 0;
     int iterFlag = 0;
     int yieldFlag = 0;
+    int syncFlag = 0;
     char* threadopt = NULL;
     char* iteropt = NULL;
     char* yieldopt = NULL;
@@ -81,6 +98,7 @@ int main(int argc, char *argv[])
       {"threads", required_argument, 0, 't'},
       {"iterations", required_argument, 0, 'i'},
       {"yield", no_argument, 0, 'y'},
+      {"sync", required_argument, 0, 's'},
       {0,0,0,0}
     };
 
@@ -99,6 +117,10 @@ int main(int argc, char *argv[])
 	case 'y':
 	  yieldFlag = 1;
 	  break;
+	case 's':
+	  syncFlag = 1;
+	  syncopt = optarg[0];
+	  break;
         default:
           fprintf(stderr, "Proper usage of options: --port=portnum, --encrypt=filename\n");
           exit(1);
@@ -116,6 +138,9 @@ int main(int argc, char *argv[])
     if (yieldFlag)
       {
 	opt_yield = 1;
+      }
+    if (syncFlag)
+      {
       }
 
     clock_gettime(CLOCK_MONOTONIC, &start);
