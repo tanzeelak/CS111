56d90753fbafb4380d9b748acff34cd9bb0b1148
tanzeelak <tnkhan8042@gmail.com>
2017-05-09 16:00:09 -0700

    edited sort

--- labs/lab2a/SortedList.c
+++ labs/lab2a/SortedList.c
@@ -7,11 +7,8 @@
 void SortedList_insert(SortedList_t *list, SortedListElement_t *element)
 {
   if(list == NULL)
-    {
       return;
-    }
   SortedListElement_t *p = list->next;
-
   while(p != list)
     {
       if(strcmp(element->key, p->key) <= 0)
@@ -20,7 +17,6 @@ void SortedList_insert(SortedList_t *list, SortedListElement_t *element)
     }
   if(opt_yield & INSERT_YIELD)
     sched_yield();
-
   element->next = p;
   element->prev = p->prev;
   p->prev->next = element;
@@ -29,18 +25,14 @@ void SortedList_insert(SortedList_t *list, SortedListElement_t *element)
 
 int SortedList_delete(SortedListElement_t *element)
 {
-
   if(element == NULL)
       return 1;
-
   if(element->next->prev == element->prev->next)
     {
       if(opt_yield & DELETE_YIELD)
 	sched_yield();
-
       element->prev->next = element->next;
       element->next->prev = element->prev;
-
       return 0;
     }
   return 1;
@@ -48,33 +40,32 @@ int SortedList_delete(SortedListElement_t *element)
 
 SortedListElement_t *SortedList_lookup(SortedList_t *list, const char *key)
 {
-  if(list == NULL || key == NULL) { return NULL; }
-  SortedListElement_t *curr = list->next;
-  while(curr != list)
+  if(list == NULL) 
+    return NULL;
+  SortedListElement_t *p = list->next;
+  while(p != list)
     {
-      if(strcmp(curr->key, key) == 0)
-	return curr;
+      if(strcmp(p->key, key) == 0)
+	return p;
       if(opt_yield & LOOKUP_YIELD)
 	sched_yield();
-      curr = curr->next;
+      p = p->next;
     }
   return NULL;
 }
 
 int SortedList_length(SortedList_t *list)
 {
-  int count = 0;
   if(list == NULL)
-    {
       return -1;
-    }
-  SortedListElement_t *curr = list->next;
-  while(curr != list)
+  int cnt = 0;
+  SortedListElement_t *p = list->next;
+  while(p != list)
     {
-      count++;
+      cnt++;
       if(opt_yield & LOOKUP_YIELD)
 	sched_yield();
-      curr = curr->next;
+      p = p->next;
     }
-  return count;
+  return cnt;
 }
